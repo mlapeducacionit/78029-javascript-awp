faefe69dd040bfd966a52793fa3624c7b9100ede
Maximiliano Príncipe <mlapeducacionit@gmail.com>
2025-11-26 20:52:54 -0300

    Terminado la lógica del menú hamburguesa, el botón cierre y el overlay

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
 import './style.css'
+
+const overlay = document.getElementById('overlay')
+const sidebar = document.getElementById('sidebar')
+const closeButton = document.getElementById('close-sidebar')
+const toggleBotton = document.getElementById('toogle-sidebar')
+
+//console.log(overlay);
+//console.log(sidebar);
+//console.log(closeSidebar);
+//console.log(toogleSidebar);
+
+const openSidebar = () => {
+    console.log('openSidebar')
+    sidebar.classList.remove('-translate-x-full')
+    overlay.classList.remove('hidden')
+}
+
+const closeSidebar = () => {
+    console.log('closeSidebar')
+    sidebar.classList.add('-translate-x-full')
+    overlay.classList.add('hidden')
+}
+
+toggleBotton.addEventListener('click', openSidebar)
+closeButton.addEventListener('click', closeSidebar)
+overlay.addEventListener('click', closeSidebar)
